e6dc56ee99dd06a3b5cff0f523b482ffb88d8114
dedsec <notdedsec@gmail.com>
2026-01-18 22:22:06 +0530

    49: fuck this sign

--- 49/jjk_49.py
+++ 49/jjk_49.py
@@ -1,3 +1,6 @@
+from vsmuxtools import FileInfo
+from vstools import match_clip, replace_ranges
+
 from project_module.source.getter import get_episode
 from project_module.video.encoder import run_encode
 from project_module.video.filters import antialias, deband, denoise, grain, merge, rescale
@@ -5,6 +8,10 @@ from project_module.video.filters import antialias, deband, denoise, grain, merg
 episode = get_episode('49')
 
 src = merge(episode)
+
+MASK = FileInfo('49\\jjk_49_22010_clean.png')
+mask = match_clip(MASK.init(), src, length=True)
+src = replace_ranges(src, mask, ranges=[(22010, 22119), (22139, 22143), (22162, 22184), (22208, 22224), (22243, 22271), (22288, 22310), (22325, 22336), (22349, 22489)])
 
 rsc = rescale(src)
 dns = denoise(rsc)
