15d75da1c3701eaee989856787834910c797eb14
dedsec <notdedsec@gmail.com>
2026-01-24 18:16:38 +0530

    51: encode

---
+++ 51/jjk_51.py
@@ -0,0 +1,17 @@
+from project_module.source.getter import get_episode
+from project_module.video.encoder import run_encode
+from project_module.video.filters import antialias, deband, denoise, grain, merge, rescale
+
+episode = get_episode('51')
+
+src = merge(episode)
+
+rsc = rescale(src)
+dns = denoise(rsc)
+aaa = antialias(dns)
+dbn = deband(aaa)
+grn = grain(dbn)
+
+
+if __name__ == '__main__':
+    run_encode(episode, grn)
